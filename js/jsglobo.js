9389984e5675a94e510a1576ebcbfe1302cd51b7
Bravo Moran Sergio Francisco <fbravo@iMac-de-informatica.local>
2019-09-16 18:18:12 -0500

    comenze a hacer la programación de javascript

---
+++ js/jsglobo.js
@@ -0,0 +1,8 @@
+var clickPun = Math.floor((Math.random() * 10) + 1);
+var globoJuego = document.getElementById('globoJuego');
+var inflar = document.getElementById('inflar');
+
+
+function cuentaClicks(){
+    globoJuego.style.fontSize = '12px';
+}
