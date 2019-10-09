4ff288884ac9df8380fce31d7e9a3baefc3db39f
mucacran <el.mucacran.rasta@hotmail.com>
2019-10-09 14:47:09 -0500

    arregle la base de datos

--- basededatos-utilidadesFuncionarios.php
+++ basededatos-utilidadesFuncionarios.php
@@ -9,12 +9,9 @@ define('DB_USER_TBL','cfn_formulariofuncionario'); // nombre de la tabla
 //variables
     $nombre1 =      isset($_POST["nombreCompleto"]) ? $_POST["nombreCompleto"] : '';
     $edad2=	        isset($_POST["edad"])           ? $_POST["edad"] : '';
-    $economia3 =	isset($_POST["economia"])       ? $_POST["economia"] : '';
     $experiencia =	isset($_POST["experiencia"])    ? $_POST["experiencia"] : '';
     $trabajo =	    isset($_POST["trabajo"])        ? $_POST["trabajo"] : '';
-    $instruxion=	isset($_POST["instruccion"])    ? $_POST["instruccion"] : '';
-    $emailform =	isset($_POST["emailFormulario"])? $_POST["emailFormulario"] : '';
-    
+    $instruxion=	isset($_POST["instruccion"])    ? $_POST["instruccion"] : '';    
  
 $conectarse = new mysqli(BD_HOST,BD_USER,DB_PASSWORD,BD_DATABASE);
 
@@ -24,7 +21,7 @@ if($conectarse->connect_error)
     exit;
 }
 
-$baseDeDatos="INSERT INTO cfn_formulariofuncionario(id,nombrecompleto,rangodeedad,sectoreconomico,tiempoexperiencia,tipodetrabajo,niveldeinstruccion,email) VALUES(NULL,'$nombre1','$edad2','$economia3','$experiencia','$trabajo','$instruxion','$emailform')";
+$baseDeDatos="INSERT INTO cfn_formulariofuncionario(id,nombrecompleto,rangodeedad,tiempoexperiencia,tipodetrabajo,niveldeinstruccion) VALUES(NULL,'$nombre1','$edad2','$experiencia','$trabajo','$instruxion')";
 
 $resultado = $conectarse->query($baseDeDatos);
 
