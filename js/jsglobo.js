baf71b4301e60cf558d30d5209fb3bc1c60ae970
mucacran <el.mucacran.rasta@hotmail.com>
2019-10-23 06:22:35 -0500

    vamos avances

--- js/jsglobo.js
+++ js/jsglobo.js
@@ -92,41 +92,53 @@ function prinPantallaNumero(contador)
 //https://codepen.io/julkapuk/pen/vjpZRW
 /********************CRONOMETRO************************/
 
+// $('#empezar').on('click',function()
+// {
+//     var conteo = 0;
+//     var segundo = 0;
+//     var minuto = 0;
+//     var cronometroCorriendo;
+    
+//     setInterval(function()
+//     {
+//         if(conteo==60)
+//         {
+//             conteo = 0;
+//             ++segundo;
+//         }
+//         else if(segundo == 60)
+//         {
+//             segundo = 0;
+//             ++minuto;
+//         }
+//         else
+//         {
+//             cronometroCorriendo = "Tiempo: " + minuto + " : " + segundo + " : " + conteo;
+//             if(cronometroCorriendo == "Tiempo: 1 : 0 : 1")
+//             {
+//                 return;
+//             }
+//             else
+//             {
+//                 cronometro.innerHTML = cronometroCorriendo ;
+//                 ++conteo;
+//             }
+//         }
+//     },15);
+//     $('#empezar').hide();
+// });
 $('#empezar').on('click',function()
 {
-    var conteo = 0;
-    var segundo = 0;
-    var minuto = 0;
-    var cronometroCorriendo;
-    
-    setInterval(function()
-    {
-        if(conteo==60)
-        {
-            conteo = 0;
-            ++segundo;
-        }
-        else if(segundo == 60)
-        {
-            segundo = 0;
-            ++minuto;
-        }
-        else
-        {
-            cronometroCorriendo = "Tiempo: " + minuto + " : " + segundo + " : " + conteo;
-            if(cronometroCorriendo == "Tiempo: 1 : 0 : 1")
-            {
-                return;
-            }
-            else
-            {
-                cronometro.innerHTML = cronometroCorriendo ;
-                ++conteo;
-            }
-        }
-    },15);
-    $('#empezar').hide();
+    var a = 1;
+    setTimeout("funcionando(a)",1000);
 });
+ function funcionando(a)
+ {
+    a+=1;
+    cronometro.innerHTML = a ;
+ }
+
+
 //////////////////////////////////////////////////////////
 /* Funcion que imprime por pantalla los puntos ganados  */
 //////////////////////////////////////////////////////////
